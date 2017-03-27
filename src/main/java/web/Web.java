932d8cee3d6c7b5cf4ae0e0de0eb426028fe3fbf
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2017-03-27 15:45:41 +0700

    update

--- src/main/java/web/Web.java
+++ src/main/java/web/Web.java
@@ -1,14 +1,26 @@
 package web;
-
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
-
 @Controller
 class Web {
-	
 	@RequestMapping("/")
 	String showHome() {
 		return "index";
 	}
-	
+	@RequestMapping("/login")
+	String showLogInPage() {
+		return "login";
+	}
+	@RequestMapping("/register")
+	String showRegisterPage() {
+		return "register";
+	}
+	@RequestMapping("/profile")
+	String showProfilePage() {
+		return "profile";
+	}
+	@RequestMapping("/logout")
+	String showLogOutPage() {
+		return "logout";
+	}
 }
