1f6898037fd6119bfceed64b001e97361c4c5cd0
Kookiat Suetrong <kookiatsuetrong@gmail.com>
2017-03-29 15:58:40 +0700

    Add MySQL

---
+++ schema.sql
@@ -0,0 +1,22 @@
+create database web default charset='utf8';
+create user web@'%' identified by 'java';
+grant all on web.* to web;
+use web;
+create table member(
+	code serial,
+	name varchar(200),
+	email varchar(200) unique,
+	password varchar(200)
+);
+
+insert into member(name, email, password)
+values('webmaster', 'webmaster@email.com', sha2('password', 512) );
+
+create table topic(
+	code serial,
+	title varchar(1000),
+	detail varchar(8000)
+);
+
+insert into topic(title, detail)
+values('Welcome', 'Welcome to the first topic');
--- src/main/java/web/Web.java
+++ src/main/java/web/Web.java
@@ -1,9 +1,38 @@
 package web;
+import java.sql.*;
+import java.util.*;
 import javax.servlet.http.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 @Controller
 class Web {
+	Web() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (Exception e) { }
+	}
+	
+	@RequestMapping("/test") @ResponseBody
+	LinkedList showTest() {
+		LinkedList list = new LinkedList();
+		try {
+			Connection c = DriverManager.getConnection(
+				"jdbc:mysql://35.185.137.11/web",
+				"web", "java");
+			Statement s = c.createStatement();
+			ResultSet r = s.executeQuery(
+				"select * from member");
+			while (r.next()) {
+				String email = r.getString("email");
+				list.add(email);
+			}
+			r.close(); s.close(); c.close();
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		return list;
+	}
+	
 	@RequestMapping("/")
 	String showHome() {
 		return "index";
